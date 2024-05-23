77164a40d6b5c8382d6761690bc2cf8e623ce7f9
ANDANTINO <AHT_666@mail.ru>
2024-05-23 13:04:24 +0500

    Added description of types and methods

--- src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
+++ src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
@@ -10,6 +10,11 @@ namespace Quartz;
 
 public static class QuartzServiceCollectionExtensions
 {
+    /// <summary>
+    /// Add an <see cref="QuartzHostedService"/> to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="configure">A delegate that is used to configure an <see cref="QuartzHostedServiceOptions"/>.</param>
     public static IServiceCollection AddQuartzHostedService(
         this IServiceCollection services,
         Action<QuartzHostedServiceOptions>? configure = null)
@@ -27,6 +32,12 @@ public static class QuartzServiceCollectionExtensions
     }
 
 #if NET8_0_OR_GREATER
+    /// <summary>
+    /// Add an <see cref="QuartzHostedService"/> and implementation <see cref="IQuartzHostedLifecycleService"/>> to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <typeparam name="THostedLifecycleService">Type implementing the <see cref="IQuartzHostedLifecycleService"/> interface.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="configure">A delegate that is used to configure an <see cref="QuartzHostedServiceOptions"/>.</param>
     public static IServiceCollection AddQuartzHostedService<THostedLifecycleService>(
         this IServiceCollection services,
         Action<QuartzHostedServiceOptions>? configure = null)
