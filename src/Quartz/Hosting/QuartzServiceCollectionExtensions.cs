d7f8cb7bc9b2d838c73caab6152fda2142417135
ANDANTINO <AHT_666@mail.ru>
2024-05-26 11:35:57 +0500

    Fixed PublishAot error

--- src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
+++ src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 
 #if NET8_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 #else
 using Microsoft.Extensions.Hosting;
@@ -38,7 +39,7 @@ public static class QuartzServiceCollectionExtensions
     /// <typeparam name="THostedLifecycleService">Type implementing the <see cref="IQuartzHostedLifecycleService"/> interface.</typeparam>
     /// <param name="services">The <see cref="IServiceCollection"/>.</param>
     /// <param name="configure">A delegate that is used to configure an <see cref="QuartzHostedServiceOptions"/>.</param>
-    public static IServiceCollection AddQuartzHostedService<THostedLifecycleService>(
+    public static IServiceCollection AddQuartzHostedService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THostedLifecycleService>(
         this IServiceCollection services,
         Action<QuartzHostedServiceOptions>? configure = null)
         where THostedLifecycleService : class, IQuartzHostedLifecycleService
